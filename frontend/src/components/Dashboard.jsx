3bddaa5b06ede6ef19599821d6ea16aa169939ef
Hashit <raizadaharshit2004@gmail.com>
2024-06-23 13:11:11 +0530

    frontend complte

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,1 +1,51 @@
-export const Dashoard = () => {};
+export const Dashboard = () => {
+  return (
+    <div>
+      <div className="flex justify-between p-4  h-14">
+        <div className="flex flex-col justify-center h-full ml-4 ">
+          PayTM App
+        </div>
+        <div className="flex items-center">
+          <div className="flex  justify-center h-full ml-4">Hello</div>
+          <div className="bg-slate-200 h-12 w-12 rounded-full flex justify-center  mt-1 mr-2 ml-4">
+            <div className=" flex  flex-col justify-center h-full  text-xl ">
+              U
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className=" flex ml-10">
+        <div className=" font-bold text-lg">Your balance </div>
+        <div className="font-semibold ml-4 text-lg"> Rs 10,000</div>
+      </div>
+      <div className=" flex flex-col ml-10 mt-6">
+        <div className=" font-bold text-lg">User </div>
+        <div className="my-2 mr-8 ">
+          <input
+            type="text"
+            className="w-full px-1 border border-1 border-gray-200 rounded "
+            placeholder="Search users ..."
+          />
+          <div className="flex justify-between h-8 mt-6">
+            <div className="flex items-center">
+              <div className="bg-slate-200 h-12 w-12 rounded-full flex justify-center  ">
+                <div className=" flex  flex-col justify-center h-full  text-xl ">
+                  H
+                </div>
+              </div>
+              <div className="flex  justify-center h-full ml-4">
+                Harshit Raizada
+              </div>
+            </div>
+            <div className="flex flex-col justify-center h-full  ">
+              <button className="w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5">
+                Send Money
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- frontend/src/components/Send.jsx
+++ frontend/src/components/Send.jsx
@@ -1,1 +1,35 @@
-export const Send = () => {};
+export const SendMoney = () => {
+  return (
+    <div className="bg-slate-300  flex justify-center py-10">
+      <div className="flex flex-col rounded shadow-md h-max bg-white p-8 w-96">
+        <div className="text-3xl font-bold text-center">Send Money</div>
+        <div className="flex flex-col justify-center h-full mt-20 ">
+          <div className="flex items-center">
+            <div className="bg-green-500 h-12 w-12 rounded-full flex justify-center  ">
+              <div className=" flex  flex-col justify-center h-full  text-2xl text-white ">
+                A
+              </div>
+            </div>
+            <div className="flex  justify-center h-full ml-2 font-semibold text-2xl">
+              Friend's Money
+            </div>
+          </div>
+          <div className="text-sm font-medium mt-1">Amount (in Rs)</div>
+          <input
+            type="text"
+            className="w-full px-1 border border-1 border-gray-200 rounded mt-2 h-10 "
+            placeholder="Enter Amount"
+          />
+          <div className="w-full mt-2">
+            <button
+              type="button"
+              className="w-full text-white bg-green-500 hover:bg-green-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2"
+            >
+              Initiate Transfer
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
